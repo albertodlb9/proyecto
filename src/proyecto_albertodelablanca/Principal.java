9a85f71e9c2f9c683a76dc87fedb5822e00edc21
Alberto de la Blanca <saloalberto97@gmail.com>
2024-04-28 15:36:25 +0200

    Primeros cambios en clase principal

--- src/proyecto_albertodelablanca/Principal.java
+++ src/proyecto_albertodelablanca/Principal.java
@@ -1,5 +1,6 @@
 package proyecto_albertodelablanca;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 /**
  * 
@@ -7,20 +8,83 @@ import java.util.Scanner;
  */
 public class Principal {
     
-    private Gimnasio gimnasio;
-    private CalendarioClases calendario;
+    private static Gimnasio gimnasio;
+    private static CalendarioClases calendario;
     private static Scanner sc;
     
     public static void main(String[] args) {
+        
+        creacionGimnasio();
         menu();
     }
     
     
     public static void menu(){
         
-        System.out.println("Bienvenido al sistema de gestion del gimnasio" + )
+        System.out.println("Bienvenido al sistema de gestion del gimnasio" + 0);
+        int opcion = 0;
         do{
-        
-        }while();
+            System.out.println("-----------------MENU-----------------");
+            System.out.println("1. Añadir cliente");
+            System.out.println("2. Dar de baja cliente");
+            System.out.println("3. Modificar datos de un cliente");
+            System.out.println("4. Mostrar clientes");
+            System.out.println("5. Menu gestion de clases");
+            System.out.println("6. Guardar y salir");
+            System.out.print("Elija su opcion: ");
+            try{
+                opcion = sc.nextInt();
+
+                switch(opcion){
+                    case 1:{
+                        //añadirCliente();
+                        break;
+                    }
+                    case 2:{
+                        //eliminarCliente();
+                        break;
+                    }
+                    case 3:{
+                        //modificarCliente();
+                        break;
+                    }
+                    case 4:{
+                        //mostrarClientes();
+                        break;
+                    }
+                    case 5:{
+                        //menuClases();
+                        break;
+                    }
+                    case 6:{
+                        //guardarCambios();
+                        System.out.println("Guardando cambios...");
+                        System.out.println("Saliendo del programa. Hasta luego!");
+                        break;
+                    }
+                    default :{
+                        System.err.println("Error: la opcion introducida no es correcta");
+                    }
+                }
+            }catch(InputMismatchException e){
+                System.err.println("Error: la opcion introducida es incorrecto");
+            }
+            
+        }while(opcion != 6);
     }
+    
+    public static void creacionGimnasio(){
+        gimnasio = new Gimnasio();
+        actualizacionGimnasio();
+        
+        if(gimnasio.getNombre().equals("")){
+            System.out.print("Introduzca el nombre de su gimnasio: ");
+            gimnasio.setNombre(sc.nextLine());
+        }
+    }
+    
+    public static void actualizacionGimnasio(){
+        //metodo para leer los datos del archivo y actualizar el gimnasio
+    }
+
 }
