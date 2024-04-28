6ccadb0363c0c73a86f82cc3202e95a5ce96a062
Alberto de la Blanca <saloalberto97@gmail.com>
2024-04-28 15:12:58 +0200

    Cambios en clase gimnasio y comienzo codigo principal

--- src/proyecto_albertodelablanca/Gimnasio.java
+++ src/proyecto_albertodelablanca/Gimnasio.java
@@ -10,9 +10,27 @@ import java.util.HashSet;
 public class Gimnasio {
    
     private HashSet<Cliente> gimnasio;
+    private String nombre;
 
     public Gimnasio() {
         this.gimnasio = new HashSet<Cliente>();
+        this.nombre = "";
+    }
+
+    public HashSet<Cliente> getGimnasio() {
+        return gimnasio;
+    }
+
+    public void setGimnasio(HashSet<Cliente> gimnasio) {
+        this.gimnasio = gimnasio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
     
     
--- src/proyecto_albertodelablanca/Principal.java
+++ src/proyecto_albertodelablanca/Principal.java
@@ -1,6 +1,15 @@
 package proyecto_albertodelablanca;
 
+import java.util.Scanner;
+/**
+ * 
+ * @author Alberto
+ */
 public class Principal {
+    
+    private Gimnasio gimnasio;
+    private CalendarioClases calendario;
+    private static Scanner sc;
     
     public static void main(String[] args) {
         menu();
@@ -9,5 +18,9 @@ public class Principal {
     
     public static void menu(){
         
+        System.out.println("Bienvenido al sistema de gestion del gimnasio" + )
+        do{
+        
+        }while();
     }
 }
