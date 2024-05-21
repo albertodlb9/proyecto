2fed1c9be9e7ae734fbbc757f26eb6bd3fb5cc90
Alberto de la Blanca <saloalberto97@gmail.com>
2024-05-21 21:34:35 +0200

    mostrar clases por dia

--- src/proyecto_albertodelablanca/ClaseDAO.java
+++ src/proyecto_albertodelablanca/ClaseDAO.java
@@ -6,6 +6,8 @@ package proyecto_albertodelablanca;
  * @author Alberto
  */
 import java.sql.*;
+import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class ClaseDAO {
     
@@ -15,5 +17,23 @@ public class ClaseDAO {
         this.conexion = conexion;
     }
     
-    
+    public void extraerClasesPorDia() throws SQLException{
+        
+        ArrayList<ArrayList> semana = new ArrayList<>();
+        for(int i = 0; i < 7; i++){
+            semana.add(new ArrayList<>());
+        }
+        String sql = "SELECT clases.*, calendario_clases.horaInicio, calendario_clases.horaFinal, calendario_clases.nombre FROM clases LEFT JOIN calendario_clases ON clases.idClase = calendario_clases.idClase;";
+        
+            PreparedStatement statement = conexion.prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            
+            while(rs.next()){
+                int idClase = rs.getInt("idClase");
+                String nombre = rs.getString("nombre");
+                String descripcion = rs.getString("descripcion");
+                //LocalTime horaInicio = LocalTime.;
+            
+        }
+    }
 }
